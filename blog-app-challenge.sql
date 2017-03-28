c19f2e7dccb4cbbb4a0f9734eaee6fce07321707
cfv7 <hq@colin.red>
2017-03-28 10:00:14 -0400

    added Tags join

--- blog-app-challenge.sql
+++ blog-app-challenge.sql
@@ -43,11 +43,20 @@ CREATE TABLE posts(
   user_id integer REFERENCES users,
   title text NOT NULL,
   content text NOT NULL,
-  tags tag_id,
-  tag_id integer REFERENCES tags,
   comment comment_id,
   comment_id REFERENCES comments
 ); 
+
+CREATE TABLE posts_tags(
+  id serial PRIMARY KEY,
+  post_id integer REFERENCES posts,
+  tag_id integer REFERENCES tags
+);
+
+INSERT INTO posts_tags
+  (post_id, tag_id)
+  VALUES 
+  (1, 1), (1, 2);
 
 INSERT INTO users
   (first_name, last_name, email_address, screen_name)
@@ -63,17 +72,21 @@ INSERT INTO tags
      RETURNING id, tag;
 
 INSERT INTO posts
-  ('users_id', title, content, tags)
+  (user_id, title, content)
     VALUES
-    ( author , 'What''s the Deal with Airline food?!', 'It''s no good!', 'funny')
-     RETURNING 'users_id', title, content, tags;
+    ( 1, 'What''s the Deal with Airline food?!', 'It''s no good!')
+     RETURNING user_id, title, content;
 
-SELECT
-    users.id as "users_id",
-    comments.id as "comment_id",
-    posts.id as "post_id"
+SELECT posts.id, tags.tag FROM posts
+  JOIN posts_tags ON posts_tags.post_id=posts.id
+  JOIN tags ON posts_tags.tag_id=tags.id;
 
-    FROM users
-    INNER JOIN posts
-    ON posts.post_id = posts.id
-    LIMIT 5;
+-- SELECT
+--     users.id as "users_id",
+--     comments.id as "comment_id",
+--     posts.id as "post_id"
+
+--     FROM users
+--     INNER JOIN posts
+--     ON posts.post_id = posts.id
+--     LIMIT 5;
